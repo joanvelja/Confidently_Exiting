6c7fd001f987bfdebeaad88b71a63c3109c56fe4
Matteo Nulli <matteo.nulli@gmail.com>
2024-05-29 19:55:47 +0200

    put the flops calculations

--- src/scripts/run_sum_samsum.sh
+++ src/scripts/run_sum_samsum.sh
@@ -35,7 +35,7 @@
 
 
 CUDA_VISIBLE_DEVICES=0 python -m run_summarization \
-    --model_name_or_path google-t5/t5-large \
+    --model_name_or_path jvelja/t5-samsum \
     --do_eval \
     --dataset_name samsum \
     --output_dir ./save/jvelja_t5-large/ \
@@ -46,13 +46,15 @@ CUDA_VISIBLE_DEVICES=0 python -m run_summarization \
     --predict_with_generate \
     --source_prefix "summarize: " \
     --use_early_exit True \
-    --exit_conf_type JSD_contrastive_confidence \
+    --exit_conf_type softmax \
     --exit_conf_threshold 0.9 \
     --exit_min_layer 19 \
     --max_eval_samples 100 \
     --include_inputs_for_metrics True \
     --use_auth_token True \
-    # --type_vocab_reduct fixed \
+    --count_flops True \
+    # --type_vocab_reduct decaying \
+
 
 
     # python run_summarization.py \
